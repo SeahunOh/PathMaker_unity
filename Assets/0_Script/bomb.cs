6d7be060a92d26487b6d800643e6001aebd06722
Sangyun <strangeyun@gmail.com>
2016-06-08 23:30:24 +0900

    bombfire

---
+++ Assets/0_Script/bomb.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bomb : MonoBehaviour {
+
+	public GameObject bombfire;
+
+	public int range = 1;
+	public bool isFire = false;
+	public int startTime;
+	//public int pTime;
+	//private float prevTime;
+
+	// Use this for initialization
+	void Start () {
+	//	prevTime = (int)Time.time;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if( Input.GetKeyDown( KeyCode.Space ) && !isFire){
+			Debug.Log( "Space key was pressed." );
+			startTime = (int)Time.time;
+			isFire = true;
+			fire ();
+		}
+			
+		if (isFire) {
+			Debug.Log ((int)Time.time - startTime);
+			if ((int)Time.time - startTime> 3) {
+				Destroy (gameObject);
+			}
+		}
+	}
+
+	void fire(){
+		Instantiate (bombfire, transform.position, transform.rotation);
+		Instantiate (bombfire, transform.position, transform.rotation);
+		Instantiate (bombfire, transform.position, transform.rotation);
+		Instantiate (bombfire, transform.position, transform.rotation);
+	}
+		
+}
---
+++ Assets/0_Script/bombFire.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bombFire : MonoBehaviour {
+
+
+	public float Speed = 100;
+	public int damage = 20;
+	// Use this for initialization
+	void Start () {
+		Destroy(gameObject, 10.0f);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(0.1f, 0.0f, 0.0f);
+	}
+
+	void OnTriggerEnter(Collider others)
+	{
+		if (others.CompareTag("WALL"))
+		{
+			Destroy(others);
+			Destroy(gameObject);
+		}
+	}
+}
