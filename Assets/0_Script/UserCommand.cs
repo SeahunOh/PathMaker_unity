92c8e1c8ff2a416c794ca41b97ffd6c62294af28
Sangyun <strangeyun@gmail.com>
2016-06-12 03:31:43 +0900

    스페이스 누를시 폭탄 생성

---
+++ Assets/0_Script/UserCommand.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UserCommand : MonoBehaviour {
+
+	public GameObject bomb;
+	bool fieldInBomb = false;
+	GameObject makeBomb;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (fieldInBomb) {
+			if (makeBomb == null) {
+				Debug.Log ("Bomb is not exist.");
+				fieldInBomb = false;
+			}
+		}
+		else if (Input.GetKeyDown (KeyCode.Space)) {
+			Debug.Log ("Space key was pressed.");
+			Debug.Log ("Create Bomb.");
+			fieldInBomb = true;
+			Vector3 bombPosition = transform.position;
+			bombPosition.y += 0.5f;
+			makeBomb = (GameObject)Instantiate (bomb, bombPosition, transform.rotation);
+		}
+	}
+}
