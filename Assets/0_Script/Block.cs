5fc736bb8d913544711ed4c1b141c349fee952e7
Sangyun <strangeyun@gmail.com>
2016-06-14 21:21:50 +0900

    add dest

--- Assets/0_Script/Block.cs
+++ Assets/0_Script/Block.cs
@@ -6,10 +6,12 @@ public class Block : MonoBehaviour {
 		BlockNormal = 0x01,
 		BlockNotDestroy = 0x02,
 		BlockItem = 0x04,
+		BlockDestination = 0x08,
 		GroundNormal = 0x10,
 		GroundTrap = 0x20,
 		GroundGenerator = 0x40,
 	}
+
 	GameObject cube;
 	Vector3 axis;
 	private BlockOption type;
@@ -26,8 +28,12 @@ public class Block : MonoBehaviour {
 			cube.tag = "BLOCK";
 		} else if (type == BlockOption.BlockNotDestroy) {
 			cube.tag = "NOT_DEST_BLOCK";
+		} else if (type == BlockOption.BlockDestination) {
+			cube.tag = "DESTINATION";
+			cube.AddComponent <BoxCollider>();
 		}
 	}
+
 	public GameObject getCube(){
 		return cube;
 	}
--- Assets/0_Script/CreateBlock.cs
+++ Assets/0_Script/CreateBlock.cs
@@ -5,10 +5,11 @@ public class CreateBlock : MonoBehaviour {
 	public GameObject user;
 	public GameObject bomb1;
 	public GameObject bombItem;
+	public GameObject destination;
 
 	Block[,] bl;
 	int[,] stage1_map1 = new int[10,10]{
-		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 8 },
 		{ 0, 1, 1, 1, 0, 0, 1, 1, 1, 0 },
 		{ 0, 1, 0, 0, 0, 0, 0, 3, 1, 0 },
 		{ 0, 1, 0, 1, 1, 1, 1, 0, 1, 0 },
@@ -17,7 +18,7 @@ public class CreateBlock : MonoBehaviour {
 		{ 0, 1, 0, 0, 0, 0, 0, 0, 1, 0 },
 		{ 0, 1, 0, 0, 0, 0, 0, 0, 1, 0 },
 		{ 0, 1, 1, 1, 1, 1, 1, 1, 1, 0 },
-		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+		{ 4, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
 	};
 	// Use this for initialization
 	void Start () {
@@ -38,17 +39,15 @@ public class CreateBlock : MonoBehaviour {
 		cnt = 0;
 		for (int i = 0; i < 10; i++) {
 			for (int j = 0; j < 10; j++) {
-				if (stage1_map1 [i, j] == 2) {
+				if (stage1_map1 [i, j] == 3) {
 					float x = (float)i;
 					float z = (float)j;
-					Instantiate (bomb1, new Vector3(i,30,j), transform.rotation);
+					Instantiate (bombItem, new Vector3 (i, 0, j), transform.rotation);
 					//bomb1.transform.parent = user;
-				}
-				else if (stage1_map1 [i, j] == 3) {
-					float x = (float)i;
-					float z = (float)j;
-					Instantiate (bombItem, new Vector3 (i, 1, j), transform.rotation);
-					//bomb1.transform.parent = user;
+				} else if (stage1_map1 [i, j] == 8) {
+					GameObject dest = (GameObject)Instantiate (destination, new Vector3 (i, 0.55f, j), transform.rotation);
+					dest.GetComponent<Renderer> ().material.color = Color.cyan;
+					dest.tag = "DESTINATION";
 				} else if (stage1_map1 [i, j] != 0){
 					float x = (float)i;
 					float z = (float)j;
