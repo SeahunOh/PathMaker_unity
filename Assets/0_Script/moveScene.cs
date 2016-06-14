28f43132974bf5855efa55110c20817ed178d86b
SeahunOh <shpik.korea@gmail.com>
2016-06-14 21:21:21 +0900

    SceneChange
    etc

---
+++ Assets/0_Script/moveScene.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveScene : MonoBehaviour {
+	public string sceneName;
+
+	void OnGUI(){
+		if(Input.GetKey(KeyCode.Return)){
+			Application.LoadLevel (sceneName);
+		}	
+	}
+}
---
+++ Assets/0_Script/userCamera.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class userCamera : MonoBehaviour {
+	public GameObject userCam;
+	public GameObject userCharacter;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		userCam.transform.LookAt (userCharacter.transform);
+	}
+}
