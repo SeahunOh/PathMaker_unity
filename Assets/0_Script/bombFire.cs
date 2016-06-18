d81a382f8b28712c7220c3498128bed01521a4c4
Sangyun <strangeyun@gmail.com>
2016-06-18 18:56:03 +0900

    merge sound

--- Assets/0_Script/bombFire.cs
+++ Assets/0_Script/bombFire.cs
@@ -2,18 +2,24 @@
 using System.Collections;
 
 public class bombFire : MonoBehaviour {
-
-
 	public float Speed = 100;
 	public int damage = 20;
+	public GameObject fireEffect;
+	private float prevTime;
+
 	// Use this for initialization
 	void Start () {
+		prevTime = Time.time;
 		Destroy(gameObject, 0.5f);
 	}
 
 	// Update is called once per frame
 	void Update () {
 		transform.Translate(0.1f, 0.0f, 0.0f);
+		if (Time.time - prevTime >= 0.1f) {
+			
+			prevTime = Time.time;
+		}
 	}
 
 	void OnTriggerEnter(Collider others)
@@ -21,11 +27,14 @@ public class bombFire : MonoBehaviour {
 		Debug.Log (others.tag);
 
 		if (others.CompareTag ("BLOCK")) {
+			Instantiate (fireEffect, transform.position, transform.rotation);
 			Destroy (others.gameObject);
 			Destroy (gameObject);
 		}
 		else if(others.CompareTag("USER")){
 			Destroy(others.gameObject);
+			Destroy (gameObject);
+			Application.LoadLevel ("GameOVer");
 		}
 	}
 }
