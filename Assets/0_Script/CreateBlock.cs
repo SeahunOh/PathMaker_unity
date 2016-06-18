38e09b4a2803f716d29f95995a41cdb34cfb014e
SeahunOh <shpik.korea@gmail.com>
2016-06-18 19:49:38 +0900

    Puhahaha
    123212322221

--- Assets/0_Script/CreateBlock.cs
+++ Assets/0_Script/CreateBlock.cs
@@ -10,18 +10,20 @@ public class CreateBlock : MonoBehaviour {
 	public GameObject GrassBlock;
 	public GameObject DestroyBlock;
 	Block[,] bl;
+	int stage;
 	/*
 	 *	0 : empty block 
 	 *  1 : destroy block
 	 *  2 : non destroy block
 	 * 	3 : bomb item
 	 *  8 : destination
+	 *  9 : Character
 	*/
 	int[,] stage1_map1 = new int[10,10]{
 		{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
 		{ 1, 0, 2, 2, 2, 2, 2, 2, 2, 1 },
 		{ 0, 2, 2, 3, 2, 3, 2, 0, 1, 0 },
-		{ 0, 2, 0, 0, 2, 0, 2, 0, 2, 0 },
+		{ 0, 2, 0, 0, 2, 9, 2, 0, 2, 0 },
 		{ 0, 2, 0, 2, 2, 0, 2, 0, 2, 0 },
 		{ 0, 2, 0, 1, 0, 0, 2, 0, 2, 0 },
 		{ 0, 2, 2, 2, 1, 2, 2, 0, 2, 0 },
@@ -29,9 +31,23 @@ public class CreateBlock : MonoBehaviour {
 		{ 2, 2, 2, 2, 0, 2, 2, 2, 2, 0 },
 		{ 3, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
 	};
+
+	int[,] stage2_map1 = new int[10,10]{
+		{2,0,0,1,0,2,3,2,3,2},
+		{2,3,2,0,0,2,1,2,0,2},
+		{2,2,3,2,9,2,1,2,0,2},
+		{2,2,0,2,0,2,0,0,1,2},
+		{2,2,0,0,0,0,0,2,3,2},
+		{2,2,2,2,1,2,1,2,2,2},
+		{2,2,2,0,0,2,1,0,0,2},
+		{2,3,2,1,0,0,2,2,1,2},
+		{2,0,1,0,2,1,0,0,1,2},
+		{2,2,2,2,2,2,2,2,8,2}
+	};
+		
 	// Use this for initialization
 	void Start () {
-		
+		stage = 1;
 		bl = new Block[100, 2];
 		int cnt = 0;
 		for (int i = 0; i < 10; i++) {
@@ -42,40 +58,37 @@ public class CreateBlock : MonoBehaviour {
 			}
 		}
 		cnt = 0;
-		for (int i = 0; i < 10; i++) {
-			for (int j = 0; j < 10; j++) {
-				if (stage1_map1 [i, j] == 4) {
-					GameObject up= GameObject.CreatePrimitive(PrimitiveType.Plane);
-					up.transform.localScale= new Vector3(0.1f, 0.1f, Mathf.Sqrt(2)/10.0f);
-					up.transform.Rotate(45.0f, 90.0f, 0.0f);
-					up.transform.position = new Vector3 (i, 1, j);
-				}
-				else if (stage1_map1 [i, j] == 3) {
-					float x = (float)i;
-					float z = (float)j;
-					Instantiate (bombItem, new Vector3 (i, 1, j), transform.rotation);
-					//bomb1.transform.parent = user;
-				} else if (stage1_map1 [i, j] == 8) {
-					GameObject dest = (GameObject)Instantiate (destination, new Vector3 (i, 0.55f, j), transform.rotation);
-					dest.GetComponent<Renderer> ().material.color = Color.cyan;
-					dest.tag = "DESTINATION";
-				} else if (stage1_map1 [i, j] == 2){
-					float x = (float)i;
-					float z = (float)j;
-					bl[cnt,1] = new Block (x, 1, z, stage1_map1[i,j],notDestroyBlock);
-				}else if (stage1_map1 [i, j] == 1){
-					float x = (float)i;
-					float z = (float)j;
-					bl[cnt,1] = new Block (x, 1, z, stage1_map1[i,j],DestroyBlock);
+		if(stage==1){
+			for (int i = 0; i < 10; i++) {
+				for (int j = 0; j < 10; j++) {
+					if (stage1_map1 [i, j] == 4) {
+						GameObject up = GameObject.CreatePrimitive (PrimitiveType.Plane);
+						up.transform.localScale = new Vector3 (0.1f, 0.1f, Mathf.Sqrt (2) / 10.0f);
+						up.transform.Rotate (45.0f, 90.0f, 0.0f);
+						up.transform.position = new Vector3 (i, 1, j);
+					} else if (stage1_map1 [i, j] == 3) {
+						float x = (float)i;
+						float z = (float)j;
+						Instantiate (bombItem, new Vector3 (i, 1, j), transform.rotation);
+						//bomb1.transform.parent = user;
+					} else if (stage1_map1 [i, j] == 8) {
+						GameObject dest = (GameObject)Instantiate (destination, new Vector3 (i, 0.55f, j), transform.rotation);
+						dest.GetComponent<Renderer> ().material.color = Color.cyan;
+						dest.tag = "DESTINATION";
+					} else if (stage1_map1 [i, j] == 2) {
+						float x = (float)i;
+						float z = (float)j;
+						bl [cnt, 1] = new Block (x, 1, z, stage1_map1 [i, j], notDestroyBlock);
+					} else if (stage1_map1 [i, j] == 1) {
+						float x = (float)i;
+						float z = (float)j;
+						bl [cnt, 1] = new Block (x, 1, z, stage1_map1 [i, j], DestroyBlock);
+					} else if (stage1_map1 [i, j] == 9) {
+						Instantiate (user, new Vector3(i,1,j), transform.rotation);
+					}
 				}
 			}
 		}
-		Vector3 userPosition = transform.position;
-		userPosition.x += 5;
-		userPosition.y += 1;
-		userPosition.z += 4;
-
-		Instantiate (user, userPosition, transform.rotation);
 	}
 	
 	// Update is called once per frame
